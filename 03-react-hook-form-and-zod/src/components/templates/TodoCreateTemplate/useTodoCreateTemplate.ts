9df08281184100c775490285929ca54457b8cc29
ikd-gifu <oki19ki@gmail.com>
2026-02-01 16:00:31 +0900

    todoCreateにZodによるバリデーションを追加

--- 03-react-hook-form-and-zod/src/components/templates/TodoCreateTemplate/useTodoCreateTemplate.ts
+++ 03-react-hook-form-and-zod/src/components/templates/TodoCreateTemplate/useTodoCreateTemplate.ts
@@ -1,16 +1,27 @@
-import { useState, useCallback, ChangeEvent } from 'react';
+import { useCallback } from 'react';
 import { useNavigate } from 'react-router';
 import { useTodoContext } from '../../../hooks/useTodoContext';
 import { NAV_ITEMS } from '../../../constants/navigation';
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const TodoCreateFormSchema = z.object({
+  title: 
+    z
+    .string()
+    .min(1, { message: 'タイトルは必須です' })
+    .max(20, { message: 'タイトルは20文字以内で入力してください' }),
+  content:
+    z
+    .string()
+    .optional(),
+});
 
 // useFormはフォーム全体の型を明示しないと、
 // handleSubmitやerrorsの型が曖昧になりやすいので、フォーム全体の型定義を作成
-type TodoCreateFormValues = {
-  title: string;
-  // string | undefined と同義
-  content?: string;
-};
+// 型をスキーマから拡張するためにz.inferを使用して型エイリアスを定義
+type TodoCreateFormValues = z.infer<typeof TodoCreateFormSchema>;
 
 /**
  * TodoCreateTemplateのページ固有UI状態管理
@@ -20,17 +31,13 @@ type TodoCreateFormValues = {
 export const useTodoCreateTemplate = () => {
   const navigate = useNavigate();
   const { handleCreateTodo } = useTodoContext();
-  
-  // フォーム入力値の状態管理
-  // useState('')で自動で型推論されるので、型注釈は不要
-  // const [title, setTitle] = useState('');
-  // const [content, setContent] = useState('');
 
   const {
     control, // コンポーネントを登録するためのメソッドを含む
     handleSubmit, // フォームの検証が成功した場合にフォーム データを送信するための関数
     formState: { errors }, // フォーム全体の状態に関する情報を含む フィールドエラーを持つオブジェクト
   } = useForm<TodoCreateFormValues>({
+    resolver: zodResolver(TodoCreateFormSchema), // Zodの検証をRHFに統合
     defaultValues: {
       title: '',
       content: undefined,
@@ -39,24 +46,6 @@ export const useTodoCreateTemplate = () => {
 
   // タイトル入力フィールドの状態管理、内容入力フィールドの状態管理
   // フォーム送信とTodoの実際の作成処理を分離
-  // UIハンドラーをtemplateから移動
-  /**
-   * title変更処理
-   * @param {ChangeEvent<HTMLInputElement>} e - 入力イベント
-   */
-  // const onChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-  //   setTitle(e.target.value);
-  // }, []);
-
-  /**
-   * content変更処理
-   * @param {ChangeEvent<HTMLTextAreaElement>} e - 入力イベント
-   */
-  // const onChangeContent = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
-  //   setContent(e.target.value);
-  // }, []);
-
-
   const handleCreateSubmit = handleSubmit(
     useCallback(
       (values: TodoCreateFormValues) => {
